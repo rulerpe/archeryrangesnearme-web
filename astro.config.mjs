1d4646f50cffedc81acd768637595a98e77d2b27
Peter Chen <peterchen@mac.lan>
2026-02-22 21:42:44 -0800

    feat: scaffold Astro project with all dependencies

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
+
+export default defineConfig({
+  site: 'https://archeryrangesnearme.com',
+  integrations: [
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push', 'gtag'],
+      },
+    }),
+  ],
+});
